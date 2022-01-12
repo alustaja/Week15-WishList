36edda89594c880221141420139fa6375a5d829e
Mati Männa <mati.manna@kehtna.edu.ee>
2022-01-12 22:41:13 +0200

    add wishlist project

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Wish = require('./model/wish.js');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    Wish.fetchAllWishes(wishesFromFile => {
+        console.log(wishesFromFile);
+        res.render('index', {myWishes: wishesFromFile}); 
+    });  
+});
+
+app.post('/wish', (req, res) => {
+    let userData = req.body.userWish;
+
+    let newWish = new Wish(userData);
+    newWish.saveWish();
+    res.redirect('/');
+});
+
+const port = 5000;
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server has started.");
+});
